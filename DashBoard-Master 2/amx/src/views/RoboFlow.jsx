db4c97fc098fb73f3633d12e84d19d9cbb771ba2
EkfrazoMohammed <mohammed@ekfrazo.in>
2024-08-30 17:51:54 +0530

    updated

--- DashBoard-Master 2/amx/src/views/RoboFlow.jsx
+++ DashBoard-Master 2/amx/src/views/RoboFlow.jsx
@@ -124,9 +124,9 @@ import Frame from "react-frame-component";
 function RoboFlow() {
   const myurl = process.env.PUBLIC_URL + "/roboflow.html";
 
-  const [toggleSection ,setTogglesection]=useState(true)
+  const [toggleSection, setTogglesection] = useState(true)
   const videoRef = useRef(null);
-  const [video,setVideo]=useState(null)
+  const [video, setVideo] = useState(null)
   const [socket, setSocket] = useState(null);
   const [isLoading, setIsLoading] = useState(false);
   console.log("object")
@@ -142,7 +142,7 @@ function RoboFlow() {
   const handlePost = async () => {
     try {
       const url = "https://your-api-url";
-      const post = await axios.post(url,{video});
+      const post = await axios.post(url, { video });
       console.log(post);
     } catch (error) {
       alert(error);
@@ -179,59 +179,61 @@ function RoboFlow() {
       setIsLoading(false);
     }
   };
-  console.log(video,'<<<')
+  console.log(video, '<<<')
   return (
     <>
- 
-      <div className="content">
-        <Row style={{justifyContent:'center'}}>
 
-          <Col lg="6" md="12" style={{height:'90vh'}}>
+      <div className="content">
+        <Row style={{ justifyContent: 'center' }}>
+
+          <Col lg="6" md="12" style={{ height: '90vh' }}>
             {
               toggleSection ?
-              <>       
-          <iframe
-          title="iframe"
-          style={{
-            width: "100%",
-            height: "87%",
-            border: "none",
-            outline: "none",
-            overflow:'hidden'
-          }}
-          src={myurl}
-          id="iframeId"
-          ></iframe>
-    <div className="file-input">
-        <input
-          type="file"
-          id="file-input"
-          className="file-input__input"
-          onChange={handleFileChange}
-          accept="video/*"
-        />
-        <label className="file-input__label" htmlFor="file-input">
-          <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="upload" className="svg-inline--fa fa-upload fa-w-16" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
-            <path fill="currentColor" d="M296 384h-80c-13.3 0-24-10.7-24-24V192h-87.7c-17.8 0-26.7-21.5-14.1-34.1L242.3 5.7c7.5-7.5 19.8-7.5 27.3 0l152.2 152.2c12.6 12.6 3.7 34.1-14.1 34.1H320v168c0 13.3-10.7 24-24 24zm216-8v112c0 13.3-10.7 24-24 24H24c-13.3 0-24-10.7-24-24V376c0-13.3 10.7-24 24-24h136v8c0 30.9 25.1 56 56 56h80c30.9 0 56-25.1 56-56v-8h136c13.3 0 24 10.7 24 24zm-124 88c0-11-9-20-20-20s-20 9-20 20 9 20 20 20 20-9 20-20zm64 0c0-11-9-20-20-20s-20 9-20 20 9 20 20 20 20-9 20-20z"></path>
-          </svg>
-          <span>Upload file</span>
-        </label>
-      </div>
-              </>
-            :
-          <>
-            <div onClick={()=>setTogglesection(true)} className="" style={{backgroundImage: 'linear-gradient(to bottom left, #FD6585, #FA742B, #FD6585)',
-  boxShadow: '0px 0px 2px rgba(0, 0, 0, 0.25)',color:'#fff',textAlign:'center',padding:'0.5rem',width:'80px',borderRadius:'5px',cursor:'pointer',fontWeight:'600'}}>BACK</div>
-            <video ref={videoRef} style={{width:'50%',height:'500px'}} controls autoPlay />
-            {/* <video src={video} style={{width:'100%',height:'60%'}} controls autoPlay /> */}
-          </>
-          }
+                <>
+                  <iframe
+                    title="iframe"
+                    style={{
+                      width: "100%",
+                      height: "87%",
+                      border: "none",
+                      outline: "none",
+                      overflow: 'hidden'
+                    }}
+                    src={myurl}
+                    id="iframeId"
+                  ></iframe>
+                  <div className="file-input">
+                    <input
+                      type="file"
+                      id="file-input"
+                      className="file-input__input"
+                      onChange={handleFileChange}
+                      accept="video/*"
+                    />
+                    <label className="file-input__label" htmlFor="file-input">
+                      <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="upload" className="svg-inline--fa fa-upload fa-w-16" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+                        <path fill="currentColor" d="M296 384h-80c-13.3 0-24-10.7-24-24V192h-87.7c-17.8 0-26.7-21.5-14.1-34.1L242.3 5.7c7.5-7.5 19.8-7.5 27.3 0l152.2 152.2c12.6 12.6 3.7 34.1-14.1 34.1H320v168c0 13.3-10.7 24-24 24zm216-8v112c0 13.3-10.7 24-24 24H24c-13.3 0-24-10.7-24-24V376c0-13.3 10.7-24 24-24h136v8c0 30.9 25.1 56 56 56h80c30.9 0 56-25.1 56-56v-8h136c13.3 0 24 10.7 24 24zm-124 88c0-11-9-20-20-20s-20 9-20 20 9 20 20 20 20-9 20-20zm64 0c0-11-9-20-20-20s-20 9-20 20 9 20 20 20 20-9 20-20z"></path>
+                      </svg>
+                      <span>Upload file</span>
+                    </label>
+                  </div>
+                </>
+                :
+                <>
+                  <div onClick={() => setTogglesection(true)} className="" style={{
+                    backgroundImage: 'linear-gradient(to bottom left, #FD6585, #FA742B, #FD6585)',
+                    boxShadow: '0px 0px 2px rgba(0, 0, 0, 0.25)', color: '#fff', textAlign: 'center', padding: '0.5rem', width: '80px', borderRadius: '5px', cursor: 'pointer', fontWeight: '600'
+                  }}>BACK</div>
+                  <video ref={videoRef} style={{ width: '50%', height: '500px' }} controls autoPlay />
+                  {/* <video src={video} style={{width:'100%',height:'60%'}} controls autoPlay /> */}
+                </>
+            }
           </Col>
-                            
-        
 
-        <Col lg="6" md="12">
-            <Card className="card-tasks" style={{height:"auto"}}>
+
+
+          <Col lg="6" md="12">
+            <Card className="card-tasks" style={{ height: "auto" }}>
               <CardHeader>
                 <h6 className="title d-inline">Plugins (4)</h6>
                 <p className="card-category d-inline"> </p>
@@ -407,7 +409,7 @@ function RoboFlow() {
                           </UncontrolledTooltip>
                         </td>
                       </tr>
-                    
+
                     </tbody>
                   </Table>
                 </div>
@@ -420,7 +422,7 @@ function RoboFlow() {
               </CardHeader>
               <CardBody>
                 <Table className="tablesorter" responsive>
-                <thead className="text-primary">
+                  <thead className="text-primary">
                     <tr>
                       <th>Objects</th>
                       <th className="text-center">Counts</th>
@@ -429,21 +431,21 @@ function RoboFlow() {
                   <tbody>
                     <tr>
                       <td>Monuments</td>
-                  
+
                       <td className="text-center">2</td>
                     </tr>
                     <tr>
                       <td>Trees/Plants</td>
-                  
+
                       <td className="text-center">78</td>
                     </tr>
                     <tr>
                       <td>Red objects</td>
-                  
+
                       <td className="text-center">3</td>
                     </tr>    <tr>
                       <td>Blue Objects</td>
-                  
+
                       <td className="text-center">5</td>
                     </tr>
 
@@ -452,7 +454,7 @@ function RoboFlow() {
                 </Table>
               </CardBody>
             </Card>
-            </Col>
+          </Col>
         </Row>
       </div>
     </>
